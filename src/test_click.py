b5a32bdff5a8c064ba1fb097aa983b265f99ea58
John Lam <jlam@iunknown.com>
2021-03-28 23:02:37 -0700

    adding option to run a different command

--- src/test_click.py
+++ src/test_click.py
@@ -2,6 +2,7 @@ from click.testing import CliRunner
 from ez import ez
 
 runner = CliRunner()
-result = runner.invoke(ez, ['--debug', 'env', 'run', '-n', 'pytorch-tutorials',
-                            '-g', 'https://github.com/jflam/pytorch-tutorials'])
+result = runner.invoke(ez, ['--debug', 'vm', 'ssh'])
+# result = runner.invoke(ez, ['--debug', 'env', 'run', '-n', 'pytorch-tutorials',
+#                             '-g', 'https://github.com/jflam/pytorch-tutorials'])
 print(result)
