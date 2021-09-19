f4a5409d6bdd1cfde1a9705c8f5cc23128ef8195
John Lam <jlam@iunknown.com>
2021-09-19 07:15:51 -0700

    fix: updating setup.py with module and external dependencies

--- src/setup.py
+++ src/setup.py
@@ -3,8 +3,12 @@ from setuptools import setup
 setup(
     name='ez',
     version='0.1',
-    py_modules=['ez'],
-    install_requires=['Click'],
+    py_modules=['ez', 
+                'workspace_commands', 
+                'env_commands', 
+                'compute_commands', 
+                'azutil'],
+    install_requires=['Click', 'rich'],
     entry_points='''
         [console_scripts]
         ez=ez:ez
