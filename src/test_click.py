c21db748cc22c25a90fdded69b96beb31177694c
John Lam <jlam@iunknown.com>
2021-10-30 07:24:31 -0700

    wip: updating test case

--- src/test_click.py
+++ src/test_click.py
@@ -1,11 +1,11 @@
+import os 
+
 from click.testing import CliRunner
 from ez import ez
 
+# Set current working dir
+os.chdir(os.path.expanduser("~/src/"))
+current_dir = os.getcwd()
 runner = CliRunner()
-# result = runner.invoke(ez, ['--debug', 'vm', 'ssh'])
-# result = runner.invoke(ez, ['--debug', 'env', 'run', '-n', 'pytorch-tutorials',
-#                             '-g', 'https://github.com/jflam/pytorch-tutorials'])
-result = runner.invoke(ez, ['--debug', 'env', 'run', '-n', 'pytorch-tutorials',
-                            '-g', 'https://github.com/jflam/pytorch-tutorials',
-                            '--vm-name', '.'])
-print(result)
+result = runner.invoke(ez, ['--debug', 'env', 'go', '-g', 
+    'git@github.com:jflam/riiid-acp-pub'])
